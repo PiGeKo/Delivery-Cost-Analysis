6bbca5ca8edf001ca348d0699f12d6a4f9026a61
PiGeKo <piotrgkonkol@gmail.com>
2026-04-11 21:40:03 +0100

    updating comments to standardise them

--- dimensions.sql
+++ dimensions.sql
@@ -6,13 +6,13 @@ CREATE TABLE dim_customer (
   customer_id INT PRIMARY KEY,
   customer_name VARCHAR(100),
   email VARCHAR(100),
-  -- varchar as there could be a country or landline code so shouldnt keep it fixed --
+  -- varchar as there could be a country or landline code so shouldnt keep it fixed 
   phone VARCHAR(15)
 );
 CREATE TABLE dim_parcel (
   parcel_id INT PRIMARY KEY,
   parcel_type VARCHAR(50),
-  -- changed "status" to "parcel_status" as status is a keyword --
+  -- changed "status" to "parcel_status" as status is a keyword 
   parcel_status VARCHAR(50),
   category VARCHAR(50)
 );
@@ -24,7 +24,7 @@ CREATE TABLE dim_location (
 );
 CREATE TABLE dim_date (
   date_id INT PRIMARY KEY,
-  -- keeps the full day and N for number as day month year are also keywords--
+  -- keeps the full day and N for number as day month year are also keywords
   full_date DATE,
   dayN INT,
   monthN INT,
